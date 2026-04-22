2ed6dd28c4c9c439c8531db713eea4117fc5f28c
maciejbulik-afk <dbox@post.pl>
2026-04-22 15:15:37 +0200

    Fix: ipc notifications debug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(async () => ({
   clearScreen: false,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notification: resolve(__dirname, "notification.html"),
+      },
+    },
+  },
   server: {
     port: 1420,
     strictPort: true,
